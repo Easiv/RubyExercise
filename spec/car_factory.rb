486feb478e8cedff8852170b557bb0f1b2f7f74c
Easiv <easiv89@gmail.com>
2018-09-03 13:02:51 +0200

    8/18 passes now

--- spec/car_factory.rb
+++ spec/car_factory.rb
@@ -1,4 +1,6 @@
 require 'pry'
+require 'car_factory/car'
+
 class CarFactory
   SUPPORTED_BRANDS = ["Fiat", "Lancia", "Ford", "Subaru"]
 
@@ -7,6 +9,10 @@ class CarFactory
     @brands = brands.values.pop.to_s.capitalize
 
     raise UnsupportedBrandException.new("Brand not supported: '#{@brands}'") unless SUPPORTED_BRANDS.include?(@brands)
+  end
+
+  def make_car
+    Car.new(@brands)
   end
 end
 
