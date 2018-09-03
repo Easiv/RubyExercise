0c24925b0a25b1cc45a88a182208c79dab442aa9
Easiv <easiv89@gmail.com>
2018-09-03 15:17:18 +0200

    10/18

--- spec/car_factory.rb
+++ spec/car_factory.rb
@@ -6,15 +6,41 @@ class CarFactory
 
   def initialize(name, brands=nil)
     @name = name
-    @brands = brands.values.pop.to_s.capitalize
+    @brands = brands.values.pop
 
-    raise UnsupportedBrandException.new("Brand not supported: '#{@brands}'") unless SUPPORTED_BRANDS.include?(@brands)
+    if @brands.is_a?(Array)
+      @brands = brands.values.pop.map(&:to_s).map(&:capitalize!)
+
+      if (SUPPORTED_BRANDS - @brands).length == 4
+        raise UnsupportedBrandException.new("Factory does not have a brand or do not support it")
+      end
+
+    else
+      @brands = brands.values.pop.to_s.capitalize
+
+      unless SUPPORTED_BRANDS.include?(@brands)       
+        raise UnsupportedBrandException.new("Brand not supported: '#{@brands}'")
+      end
+    end
   end
 
-  def make_car
-    Car.new(@brands)
+  def make_car(car=nil)
+    if !car && !@brands.is_a?(Array)
+      car = @brands
+      Car.new(car)
+    else 
+      Car.new(car)
+    end
   end
 end
 
 class UnsupportedBrandException < Exception
 end
+
+=begin
+jezeli w supported brands nie ma danego pojedynczego stringa 
+&& (i)
+jak odejmiesz brands od supported i wynik wyjdzie 4 
+
+TO -> wywal exceptioin
+=end
